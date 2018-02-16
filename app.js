754d8b518f502fb85ea050f3d4fabe0597fadbc9
ingridsag <starlling@hotmail.com>
2018-02-16 00:19:35 -0600

    funciones v2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 //identificando los elementos
 var textRealTime = document.getElementById("text-real-time");//donde se pinta el texto en tiempo real
 var textingArea = document.getElementById("areaText");//donde el usuario escribe el texto a pintar
-var sendText =document.getElementById("sending");//botón para enviar el texto para imprimirlo
 //botones que cambiaran el texto en tiempo real
 var largeSize= document.getElementById("large");//botón para que la fuente sea grande
 var midSize= document.getElementById("medium");//botón para que la fuente sea mediana
 var smallSize= document.getElementById("small");//botón para que la fuente sea chica
 var colorText =document.getElementById("text-color");
 var backgroundColor =document.getElementById("bakg-color");
-var leftAlign= document.getElementById("left");
+var leftAlign= document.getElementById("left-text");
 var centreAlign= document.getElementById("centre");
 var rigthAlign= document.getElementById("rigth");
 
@@ -20,60 +19,52 @@ largeSize.addEventListener("click", largeFont);
 midSize.addEventListener("click", mediumFont);
 smallSize.addEventListener("click", smallFont);
 leftAlign.addEventListener("click",alignLeft);
-sendText.addEventListener("click", print);
-/*//textRealTime.addEventListener("click", );
+centreAlign.addEventListener("click", alignCentre);
+rigthAlign.addEventListener("click", aligRigth);
 
 
-centreAlign.addEventListener("click", perritos);
-rigthAlign.addEventListener("click", perritos);*/
-
-//funciones
-
+//funciones para cambiar estilos
+//función para mostrar previsualización
 function preView(){
-  textRealTime.textContent=textingArea.value;
+  textRealTime.textContent =textingArea.value;
 };
 
+//función para cambiar el color de fuente
 function colorFont(){
   var color= prompt("Elige un color");
-  textRealTime.style.color=color;
+  textRealTime.style.color =color;
 };
 
+//función para cambiar el color de fondo
 function colorBackground(){
   var color= prompt("Elige un color de fondo");
-  textRealTime.style.backgroundColor=color;
+  textRealTime.style.backgroundColor =color;
 };
 
+//función para cambiar a grande el tamaño de letra
 function largeFont(){
- textRealTime.style.fontSize="4rem";
+ textRealTime.style.fontSize ="4rem";
 };
 
+//función para cambiar a mediano el tamaño de letra
 function mediumFont(){
- textRealTime.style.fontSize="2rem";
+ textRealTime.style.fontSize ="2rem";
 };
 
+//función para cambiar a chico el tamaño de letra
 function smallFont(){
- textRealTime.style.fontSize="1rem";
+ textRealTime.style.fontSize ="1rem";
 };
 
+//función para cambiar aliniación izquierda el texto
 function alignLeft(){
- textRealTime.style.textAlign="left";
+ textRealTime.style.textAlign= "left";
+};
+//función para cambiar aliniación centrada al texto
+function alignCentre(){
+  textRealTime.style.textAlign= "center";
 };
 
-//TIENES FOTO DE LA DATA, CRÉALA en otro archivo a partir de aquí
-
-
-
-//función para jalar data.
-function pastComments () {
-  //console.log(VARIALBLE QUE CONTIENE A LA DATA)
-  for (var i=0; i<data.length; i++){
-    var
-  }
-
-pastComments();
-
-
-
-function print(){
-  textRealTime.
+function aligRigth(){
+  textRealTime.style.textAlign= "right";
 }
